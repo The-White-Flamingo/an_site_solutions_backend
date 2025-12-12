f6c857572f043f465a0b7bf0d92e774dc509c0c3
Michael Agbeyebiawo <agbeyebiawomicheal@gmail.com>
2025-12-12 02:25:04 +0000

    final commit

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.OLD_MONGO_URI,
+        await mongoose.connect(process.env.MONGO_URI,
         //      {
         //     useNewUrlParser: true,
         //     useUnifiedTopology: true
